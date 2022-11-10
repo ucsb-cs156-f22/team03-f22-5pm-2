ac695af42eef8980e1562d2adb15a1c6db0760ac
glt <glt@mail2tor.com>
2022-11-09 17:13:51 -0800

    glt - Remove some code in Organization index to pass CI
    I removed some unused imports and assignments that caused the lint
    check to fail.

--- frontend/src/main/pages/UCSBOrganizations/UCSBOrganizationsIndexPage.js
+++ frontend/src/main/pages/UCSBOrganizations/UCSBOrganizationsIndexPage.js
@@ -1,22 +1,8 @@
 import React from 'react'
-import { useBackend } from 'main/utils/useBackend'; // use prefix indicates a React Hook
 
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
-import { useCurrentUser } from 'main/utils/currentUser' // use prefix indicates a React Hook
 
 export default function UCSBOrganizationsIndexPage() {
-
-  const currentUser = useCurrentUser();
-
-  const { data: dates, error: _error, status: _status } =
-    useBackend(
-      // Stryker disable next-line all : don't test internal caching of React Query
-      ["/api/UCSBOrganization/all"],
-            // Stryker disable next-line StringLiteral,ObjectLiteral : since "GET" is default, "" is an equivalent mutation
-            { method: "GET", url: "/api/UCSBOrganization/all" },
-      []
-    );
-
   return (
     <BasicLayout>
       <div className="pt-2">
