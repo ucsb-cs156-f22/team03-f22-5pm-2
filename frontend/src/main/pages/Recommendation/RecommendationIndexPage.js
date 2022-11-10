f6c4e6ee2473a7cd77e38a27a3ed17a7e9bf1a16
Bryan Huang <b_huang@cs.ucsb.edu>
2022-11-09 17:32:10 -0800

    bh - adding recommendation index page

--- frontend/src/main/pages/Recommendation/RecommendationIndexPage.js
+++ frontend/src/main/pages/Recommendation/RecommendationIndexPage.js
@@ -1,8 +1,21 @@
-import React from "react";
+import React from 'react'
+import { useBackend } from 'main/utils/useBackend'; // use prefix indicates a React Hook
 
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+import { useCurrentUser } from 'main/utils/currentUser' // use prefix indicates a React Hook
 
 export default function RecommendationIndexPage() {
+
+  const currentUser = useCurrentUser();
+
+  const { data: recommendation, error: _error, status: _status } =
+    useBackend(
+      // Stryker disable next-line all : don't test internal caching of React Query
+      ["/api/recommendation/all"],
+            // Stryker disable next-line StringLiteral,ObjectLiteral : since "GET" is default, "" is an equivalent mutation
+            { method: "GET", url: "/api/recommendation/all" },
+      []
+    );
 
   return (
     <BasicLayout>
