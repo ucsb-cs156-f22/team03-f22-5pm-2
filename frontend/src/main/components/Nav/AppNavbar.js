fd6de7b9b0ffd10412fd8e79e39c94550dc76d5a
eduglt <72234957+eduglt@users.noreply.github.com>
2022-11-10 21:01:57 -0800

    Update AppNavbar.js

--- frontend/src/main/components/Nav/AppNavbar.js
+++ frontend/src/main/components/Nav/AppNavbar.js
@@ -97,6 +97,7 @@ export default function AppNavbar({ currentUser, systemInfo, doLogout, currentUr
 		              </NavDropdown>
 		            )
 	            }
+	      }
               {
                 hasRole(currentUser, "ROLE_USER") && (
                   <NavDropdown title="Help Requests" id="appnavbar-helprequests-dropdown" data-testid="appnavbar-helprequests-dropdown" >
