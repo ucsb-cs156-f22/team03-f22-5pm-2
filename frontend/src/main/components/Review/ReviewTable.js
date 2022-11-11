10b94be324f4e41a0b646d6e19bc0f1d4f7d0823
Sophia Cheng <sophiacheng@Sophias-MacBook-Air.local>
2022-11-11 01:44:21 -0800

    sc - adding table to list

---
+++ frontend/src/main/components/Review/ReviewTable.js
@@ -0,0 +1,68 @@
+import OurTable from "main/components/OurTable";
+// import { useBackendMutation } from "main/utils/useBackend";
+// import { cellToAxiosParamsDelete, onDeleteSuccess } from "main/utils/ReviewUtils"
+// import { useNavigate } from "react-router-dom";
+// import { hasRole } from "main/utils/currentUser";
+
+export default function ReviewTable({ menuItem, _currentUser }) {
+
+    // const navigate = useNavigate();
+
+    // const editCallback = (cell) => {
+    //     navigate(`/menuitemreview/edit/${cell.row.values.id}`)
+    // }
+
+    // Stryker disable all : hard to test for query caching
+    // const deleteMutation = useBackendMutation(
+    //     cellToAxiosParamsDelete,
+    //     { onSuccess: onDeleteSuccess },
+    //     ["/api/menuitemreview/all"]
+    // );
+    // Stryker enable all 
+
+    // Stryker disable next-line all : TODO try to make a good test for this
+    // const deleteCallback = async (cell) => { deleteMutation.mutate(cell); }
+
+    const columns = [
+        {
+            Header: 'ID',
+            accessor: 'id',
+        },
+        {
+            Header: 'Item ID',
+            accessor: 'itemID',
+        },
+        {
+            Header: 'Comments',
+            accessor: 'comments',
+        },
+        {
+            Header: 'Date Reviewed',
+            accessor: 'dateReviewed',
+        },
+        {
+            Header: 'Reviewer Email',
+            accessor: 'reviewerEmail',
+        },
+        {
+            Header: 'Stars',
+            accessor: 'stars',
+        }
+    ];
+
+    // const columnsIfAdmin = [
+    //     ...columns,
+    //     ButtonColumn("Edit", "primary", editCallback, "ReviewTable"),
+    //     ButtonColumn("Delete", "danger", deleteCallback, "ReviewTable")
+    // ];
+
+    // const columnsToDisplay = hasRole(currentUser, "ROLE_ADMIN") ? columnsIfAdmin : columns;
+
+    const columnsToDisplay = columns;
+
+    return <OurTable
+        data={menuItem}
+        columns={columnsToDisplay}
+        testid={"ReviewTable"}
+    />;
+};
